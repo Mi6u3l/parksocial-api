89ce0b873d9d21f5567baec92bdcc6dc153cb44a
Mi6u3l <miguelbragagomes@gmail.com>
2017-08-08 14:03:39 +0200

    websockets config

--- routes/parking-spot.js
+++ routes/parking-spot.js
@@ -11,13 +11,7 @@ const socketIO = require('socket.io');
 
 //testing socket.io
 const socketServer = express()
-  app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-        next();
-  })
+  .use((req, res) => res.sendStatus(200))
   .listen(process.env.WSPORT, () => console.log(`Listening on ${ process.env.WSPORT }`));
 
 const io = socketIO(socketServer);
